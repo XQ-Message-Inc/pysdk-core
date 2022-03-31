ea00336ef151975565dbe86159128247fd7de8b3
mediocretech <github@mediocretech.com>
2022-03-31 17:32:47 -0400

    fixed the validate_api_key mock return

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -8,6 +8,6 @@ from xq.api import XQAPI
 @pytest.fixture
 def mock_xqapi():
     # override checking for valid provided keys
-    with patch.object(XQAPI, "validate_api_key", return_value=(200, "api is good")):
+    with patch.object(XQAPI, "validate_api_key", return_value=True):
         xqapi = XQAPI()
         yield xqapi
