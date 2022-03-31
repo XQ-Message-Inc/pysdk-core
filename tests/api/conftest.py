ed53591cfbc45e27d72e790223a6657ec59265b9
mediocretech <github@mediocretech.com>
2022-03-31 17:42:41 -0400

    mocking api keys

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -2,12 +2,17 @@ from unittest import mock
 import pytest
 from unittest.mock import patch, MagicMock
 
-from xq.api import XQAPI
-
 
 @pytest.fixture
 def mock_xqapi():
-    # override checking for valid provided keys
-    with patch.object(XQAPI, "validate_api_key", return_value=True):
-        xqapi = XQAPI()
+
+    with mock.patch("xq.XQAPI") as mock_api:
+        mock_api.validate_api_key = MagicMock(
+            return_value=True
+        )  # override checking for valid provided keys
+        xqapi = mock_api()  # create mocked instance
+
+        xqapi.api_key = "mockapikey"  # set fake api key
+        xqapi.dashboard_api_key = "mockdashboardkey"  # set fake dashboard key
+
         yield xqapi
