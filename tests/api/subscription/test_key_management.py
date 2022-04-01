4adb54e642421630a5b773a4461e3561673d5a22
mediocretech <github@mediocretech.com>
2022-03-31 20:09:33 -0400

    tets for subscription key management

--- tests/api/subscription/test_key_management.py
+++ tests/api/subscription/test_key_management.py
@@ -1,1 +1,28 @@
-from xq.api.validation.key_management import *
+import pytest
+from unittest.mock import MagicMock
+
+from xq.api.subscription.key_management import *
+
+
+def test_create_packet_200(mock_xqapi):
+    mock_xqapi.api_post = MagicMock(return_value=(200, "mock server success"))
+    assert create_packet(
+        mock_xqapi, recipients="mock@test.com", key=b"justasupersecret"
+    )
+
+
+def test_create_packet_error(mock_xqapi):
+    mock_xqapi.api_post = MagicMock(return_value=(500, "mock server error"))
+    with pytest.raises(XQException):
+        create_packet(mock_xqapi, recipients="mock@test.com", key=b"justasupersecret")
+
+
+def test_store_packet_200(mock_xqapi):
+    mock_xqapi.api_post = MagicMock(return_value=(200, "mock server success"))
+    assert store_packet(mock_xqapi, b"mockkeypacket")
+
+
+def test_store_packet_error(mock_xqapi):
+    mock_xqapi.api_post = MagicMock(return_value=(500, "mock server error"))
+    with pytest.raises(XQException):
+        store_packet(mock_xqapi, b"mockkeypacket")
