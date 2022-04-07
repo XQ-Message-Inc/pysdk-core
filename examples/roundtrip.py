c5250ec9c2611596a6b9317516b94aedc46b334f
mediocretech <github@mediocretech.com>
2022-04-07 11:26:10 -0400

    generic example file

--- examples/roundtrip.py
+++ examples/roundtrip.py
@@ -15,8 +15,10 @@ from xq import XQ
 xq = XQ()
 
 # get user authentication token
-email = "adam@mediocretech.com"
-xq.api.authorize_user(email, "adam", "ge")  # returns success boolean
+email = input(f"Please provide the email address that will be used for authentication:")
+first_name = input(f"Please provide your first name:")
+last_name = input(f"Please provide your last name:")
+xq.api.authorize_user(email, first_name, last_name)  # returns success boolean
 
 # 2FA
 pin = input(f"Please provide the PIN sent to the email address '{email}':")
@@ -26,26 +28,25 @@ xq.api.code_validate(pin)
 new_key = xq.api.exchange_key()
 
 # create key packet
-MYSUPERSECRET = b"itissixteenbytes"
-encrypted_key_packet = xq.api.create_packet(
-    recipients="adam@mediocretech.com", key=MYSUPERSECRET
-)
+MYSUPERSECRETKEY = b"itissixteenbytes"
+encrypted_key_packet = xq.api.create_packet(recipients=[email], key=MYSUPERSECRETKEY)
 
 # store key packet
 locator_token = xq.api.add_packet(encrypted_key_packet)
 
 # encrypt something
+message_to_encrypt = "sometexttoencrypt"
+print("\nencrypting message:", message_to_encrypt)
 encrypted_message, nonce, tag = xq.encrypt_message(
-    "sometexttoencrypt",
-    key=MYSUPERSECRET,
+    message_to_encrypt,
+    key=MYSUPERSECRETKEY,
     algorithm="AES",
-    recipients=["adam@mediocretech.com"],
+    recipients=[email],
 )
 print("\nencrypted_message", encrypted_message)
 
 # get key packet by lookup
 retrieved_key_packet = xq.api.get_packet(locator_token)
-print("\nretrieved_key_packet", retrieved_key_packet)
 
 # deycrypt
 decrypted_message = xq.decrypt_message(
