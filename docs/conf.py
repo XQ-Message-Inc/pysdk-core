428ce2c2defae49b860890b5ddb136f1b7e704e0
mediocretech <github@mediocretech.com>
2022-04-07 10:42:25 -0400

    added option for sphinx coverage

--- docs/conf.py
+++ docs/conf.py
@@ -27,11 +27,7 @@ author = "XQ Message"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.coverage",
-    "sphinx.ext.napoleon",
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -68,3 +64,6 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# options for sphinx.ext.coverage
+coverage_show_missing_items = True
