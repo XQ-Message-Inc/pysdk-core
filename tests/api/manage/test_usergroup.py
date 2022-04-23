7f2299a55d6042a5ce08b86ec01c933327d774ac
mediocretech <github@mediocretech.com>
2022-04-23 09:16:29 -0400

    adding usergroup files

---
+++ tests/api/manage/test_usergroup.py
@@ -0,0 +1,5 @@
+import pytest
+from unittest.mock import MagicMock
+
+from xq.api.manage.usergroup import *
+from xq.exceptions import XQException
---
+++ xq/api/manage/usergroup.py
@@ -0,0 +1,54 @@
+##
+# WARNING: THIS IS ALL MADE UP, THERE ARE NO DOCS
+#
+# Assumed Requirements: Full CRUD on https://dashboard.xqmsg.net/v2/usergroup
+##
+
+from xq.exceptions import XQException
+from xq.api.manage import API_SUBDOMAIN
+
+
+def create_usergroup(api):
+    payload = {}
+
+    status_code, res = api.api_post("usergroup", json=payload, subdomain=API_SUBDOMAIN)
+
+    if status_code == 200:
+        return res
+    else:
+        raise XQException(message=f"Error authenticating to Dashboard: {res}")
+
+
+def get_usergroup(api):
+    params = {}
+
+    status_code, res = api.api_get("usergroup", params=params, subdomain=API_SUBDOMAIN)
+
+    if status_code == 200:
+        return res
+    else:
+        raise XQException(message=f"Error authenticating to Dashboard: {res}")
+
+
+def update_usergroup(api):
+    params = {}
+
+    status_code, res = api.api_put("usergroup", params=params, subdomain=API_SUBDOMAIN)
+
+    if status_code == 200:
+        return res
+    else:
+        raise XQException(message=f"Error authenticating to Dashboard: {res}")
+
+
+def delete_usergroup(api, usergroup_id):
+    params = {}
+
+    status_code, res = api.api_delete(
+        "usergroup", params={"id": usergroup_id}, subdomain=API_SUBDOMAIN
+    )
+
+    if status_code == 200:
+        return res
+    else:
+        raise XQException(message=f"Error authenticating to Dashboard: {res}")
