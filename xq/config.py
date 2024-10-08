f7b87b94dd25023d12c6ac19854ef13a8412a392
talpalarutraian@gmail.com <talpalarutraian@gmail.com>
2024-10-08 00:15:00 -0700

    Update to config.py to allow users to update location of .env file

--- xq/config.py
+++ xq/config.py
@@ -1,17 +1,23 @@
 import os
-from os.path import join, dirname
 from dotenv import load_dotenv
 
+# Declare global variables with default values
+API_BASE_URI = None
+API_KEY = None
+DASHBOARD_API_KEY = None
+XQ_LOCATOR_KEY = None
+
 def configure_env(dotenv_path=None):
+    global API_BASE_URI, API_KEY, DASHBOARD_API_KEY, XQ_LOCATOR_KEY
+
     if dotenv_path:
         load_dotenv(dotenv_path)
     else:
-        load_dotenv()
+        load_dotenv(os.path.join(os.getcwd(), '.env'))
+    
+    API_BASE_URI = "xqmsg.net/v2/"
+    API_KEY = os.environ.get("XQ_API_KEY")
+    DASHBOARD_API_KEY = os.environ.get("XQ_DASHBOARD_API_KEY")
+    XQ_LOCATOR_KEY = os.environ.get("XQ_LOCATOR_KEY")
 
 configure_env()
-
-# set global XQ config variables
-API_BASE_URI = "xqmsg.net/v2/"
-API_KEY = os.environ.get("XQ_API_KEY", None)
-DASHBOARD_API_KEY = os.environ.get("XQ_DASHBOARD_API_KEY", None)
-XQ_LOCATOR_KEY= os.environ.get("XQ_LOCATOR_KEY", None)
