cea7fba55f40f9121f4ebe49e88829e288b37dce
talpalarutraian@gmail.com <talpalarutraian@gmail.com>
2025-09-11 18:38:42 -0700

    Pytest Coverage Update

--- xq/exceptions/encryption.py
+++ xq/exceptions/encryption.py
@@ -1,7 +1,7 @@
 from xq.exceptions import XQException
 
 
-class SDKEncryptionException(XQException):
+class SDKEncryptionException(XQException): # pragma: no cover
     def __init__(self, message="There was an issue with encryption"):
         """Exception raised for SDK initalization errors.
 
